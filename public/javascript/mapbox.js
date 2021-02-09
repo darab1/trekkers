7da08416ae6f6c8e0412a29ad10939ffe1b84661
darab <dimarabatzis@outlook.com>
2021-02-09 18:59:17 +0200

    Create functionality for rendering the map with the locations and the location pins.

--- public/javascript/mapbox.js
+++ public/javascript/mapbox.js
@@ -11,9 +11,36 @@ mapboxgl.accessToken =
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/darab8/ckkwrg6hw56qa17qdh6fdpj4u'
-  // center: [22.502055, 40.277168],
-  // zoom: 8
+  style: 'mapbox://styles/darab8/ckkwrg6hw56qa17qdh6fdpj4u',
+  scrollZoom: false
 });
 
-const bounds = new mapboxgl.LatLngBounds();
+const bounds = new mapboxgl.LngLatBounds();
+
+tourLocations.forEach(location => {
+  const element = document.createElement('div');
+  element.className = 'location-pin';
+
+  new mapboxgl.Marker({
+    element: element,
+    anchor: 'bottom'
+  })
+    .setLngLat(location.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({ offset: 25 })
+    .setLngLat(location.coordinates)
+    .setHTML(`<p>Day ${location.day}: ${location.name}</p>`)
+    .addTo(map);
+
+  bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 120,
+    bottom: 80,
+    left: 50,
+    right: 50
+  }
+});
